61aa0e31697ab11cc27d9a322cb3866a2e85e45c
Avery Miles <MilesA@chc.edu>
2022-10-06 10:49:25 -0400

    October 6, 2022

---
+++ headers/BasicIO.h
@@ -0,0 +1,10 @@
+// include pre-processor directives
+#define FIRST_NAME "Avery"
+#define LAST_NAME "Miles"
+#define WEIGHT 209.4
+#define AGE 30
+#include <stdio.h>
+
+// include function declarations
+void output(void);
+void input(void);
